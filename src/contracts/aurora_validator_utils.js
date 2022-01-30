e1526d268f8840dcf28dc3301d3e67fc495e8433
KA <asooja@gmail.com>
2022-01-30 23:41:15 +0000

    Add example call to stake with aurora validator.

--- src/contracts/aurora_validator_utils.js
+++ src/contracts/aurora_validator_utils.js
@@ -2,6 +2,7 @@
   .ts would not allow access these methods directly. */
 
 import * as nearApi from "near-api-js";
+import { formatNearAmount } from "near-api-js/lib/utils/format";
 
 export const claimRewards = async function claimRewards(config, accountId) {
   const near = await nearApi.connect(config);
@@ -17,6 +18,28 @@ export const claimRewards = async function claimRewards(config, accountId) {
     nearApi.utils.format.parseNearAmount(
       String(nearApi.utils.format.formatNearAmount("1"))
     )
+  );
+  let res = await account.signAndSendTransaction({
+    receiverId: "aurora.pool.near",
+    actions: [action1],
+  });
+  let decodedRes = await nearApi.providers.getTransactionLastResult(res);
+  return decodedRes;
+};
+
+export const stakeNear = async function stakeNear(
+  config,
+  accountId,
+  amount_to_stake
+) {
+  console.log(amount_to_stake);
+  const near = await nearApi.connect(config);
+  const account = await near.account(accountId);
+  const action1 = nearApi.transactions.functionCall(
+    "deposit_and_stake",
+    {},
+    100000000000000,
+    amount_to_stake
   );
   let res = await account.signAndSendTransaction({
     receiverId: "aurora.pool.near",
--- src/tx_pipeline.ts
+++ src/tx_pipeline.ts
@@ -30,19 +30,19 @@ async function main() {
   // console.log(per_aurora_price_for_x_amount);
   /********************************************************************************************************************/
   /*Sample call to sell aurora tokens on ref finance*/
-  let amount_to_sell = 1;
-  let per_token_min_price_in_near = 1.2;
-  const response = await ref_finance.sellTokenTowNear(
-    constants.MAINNET_CONFIG,
-    String(process.env.USER_ACCOUNT_ID_1),
-    1395,
-    utils.get_big_num_str_rep(amount_to_sell * constants.AURORA_DENOMINATION),
-    constants.AURORA_TOKEN_ADDRESS,
-    utils.get_big_num_str_rep(
-      per_token_min_price_in_near * amount_to_sell * constants.NEAR_DENOMINATION
-    )
-  );
-  console.log(response);
+  // let amount_to_sell = 1;
+  // let per_token_min_price_in_near = 1.2;
+  // const response = await ref_finance.sellTokenTowNear(
+  //   constants.MAINNET_CONFIG,
+  //   String(process.env.USER_ACCOUNT_ID_1),
+  //   1395,
+  //   utils.get_big_num_str_rep(amount_to_sell * constants.AURORA_DENOMINATION),
+  //   constants.AURORA_TOKEN_ADDRESS,
+  //   utils.get_big_num_str_rep(
+  //     per_token_min_price_in_near * amount_to_sell * constants.NEAR_DENOMINATION
+  //   )
+  // );
+  // console.log(response);
   /********************************************************************************************************************/
   /*Sample call to unwrap wrapped nears*/
   // let near_amount_to_unwrap = 1 * constants.NEAR_DENOMINATION;
@@ -56,6 +56,15 @@ async function main() {
   // );
   // console.log(response);
   /********************************************************************************************************************/
+  /*Sample call to stake near with aurora validator*/
+  let amount_to_stake = 1 * constants.NEAR_DENOMINATION;
+  let amount_to_stake_in_str = utils.get_big_num_str_rep(amount_to_stake);
+  const response = await aurora_validator.stakeNear(
+    constants.MAINNET_CONFIG,
+    String(process.env.USER_ACCOUNT_ID_1),
+    amount_to_stake_in_str
+  );
+  console.log(response);
 }
 
 main();
